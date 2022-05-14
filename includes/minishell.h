99b72da57b4468533eb225918e5133f0488170da
anso33 <hyeon131812@gmail.com>
2022-05-14 16:22:19 +0900

    parse

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,11 +1,22 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "libft/libft.h"
+#include "../libft/libft.h"
+
+
+typedef struct s_cmd
+{
+    char    *cmd;
+    char    *str;
+    int     flag;
+    struct s_cmd    *prev;
+    struct s_cmd    *next;
+}               t_cmd;
 
 typedef struct  s_minishell
 {
     int temp;
+    t_cmd   *cmd_list;
 
 } t_minishell;
 
---
+++ src/parse/parse.c
@@ -0,0 +1,7 @@
+
+#include "../../includes/minishell.h"
+
+int parse(t_minishell mnsh, char *line)
+{
+
+}
