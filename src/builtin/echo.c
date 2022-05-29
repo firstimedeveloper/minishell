a65fa59dcd9539bb117669ebfdc4f043c797d849
anso33 <hyeon131812@gmail.com>
2022-05-30 02:59:58 +0900

    fin echo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -16,16 +16,16 @@ int	check_op(char *str) // 옵션이면 0을 옵션 아니면 1을 출력한다.
 
 void	echo_print(char **envp, char *str)
 {
-	char	*envp;
+	char	*envp_value;
 
 	if (*str == '$')
 	{
 		str++;
-		envp = ft_getenv(envp, str);	//아 이거 쓰면 안될 것 같은게 그럼 본쉘의 환경변수를 검색하게 되잖아 
-		if (!envp)
+		envp_value = ft_getenv(envp, str);
+		if (!envp_value)
 			printf("");
 		else
-			printf("%s", envp);
+			printf("%s", envp_value);
 	}
 	else
 		printf("%s", str);
@@ -33,18 +33,17 @@ void	echo_print(char **envp, char *str)
 
 int	cmd_echo(char **envp, char **argv)
 {
-	int i;
 	printf("cmd echo is called\n");
-(void) envp;
+	int i;
+
 	i = 1;	
-	while (argv[i])
-		if (!check_op(argv[1]) && !check_op(argv[i]))	//	같으면(0) -> 0! // 옵션 있으므로 다음 단어부터 출력
-			i++;
+	while (!check_op(argv[i]))	//	같으면(0) -> 0! // 옵션 있으므로 다음 단어부터 출력
+		i++;	
 	while (argv[i])
 	{
-			echo_print(envp, argv[i]);
-			if (argv[++i])		//다음 단어가 있으면 띄어쓰기 출력 하고 인덱스++
-				printf(" ");
+		echo_print(envp, argv[i]);
+		if (argv[++i])		//다음 단어가 있으면 띄어쓰기 출력 하고 인덱스++
+			printf(" ");
 	}
 	if (check_op(argv[1]))	// 값이 있으면(1이상) 옵션 없으므로 개행 넣기
 		printf("\n");
@@ -59,3 +58,5 @@ echo -nnnnnn도 동작한다.
 echo -n-n-n은 문자열로 인식한다. 
 echo -n -n -n 도 옵션으로 인식한다.
 */
+
+//////확인해봐야하는 부분 : echo -n -n -c -n dfs 에서 마지막 -n 나오는지 
