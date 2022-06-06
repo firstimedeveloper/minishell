e26be4c4681c0d26d50aa22d4f9114756659c04a
Sohyeon An <san@c2r9s1.42seoul.kr>
2022-06-06 15:19:05 +0900

    fix part related envp saving

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -1,29 +1,30 @@
 #include "minishell.h"
 
-char	**unset_del(char **envp, char *str)
+int	unset_del(char ***envp, char *str)
 {
 	int	i;
 	int	j;
 	char	**new;
 
-	i = ft_envplen(envp) - 1;	//new의 개수
+	i = ft_envplen(*envp) - 1;	//new의 개수
 	j = i;					//envp 값이 들어있는 마지막 인덱스
-	new = malloc(sizeof(char *) * (i + 1));
-//	if (!new)
-//		return (1);
+	new = (char **)malloc(sizeof(char *) * (i + 1));
+	if (!new)
+		return (1);
 	new[i] = NULL;
 	while (--i > -1)
 	{
-		if (ch_strncmp(str, get_envp_name(envp[j]), ft_strlen(get_envp_name(envp[j]))) == 0)
+		if (ch_strncmp(str, get_envp_name((*envp)[j]), ft_strlen(get_envp_name((*envp)[j]))) == 0)
 			j--;
-		new[i] = ft_strdup(envp[j]);
+		new[i] = ft_strdup((*envp)[j]);
 		j--;
 	}
-	ft_free_double(envp);
-	return (new);	
+	ft_free_double(*envp);
+	*envp = new;
+	return (0);	
 }
 
-int	cmd_unset(char **envp, char **argv)
+int	cmd_unset(t_minishell *sh, char **argv)
 {
 	printf("cmd unset is called\n");
 
@@ -32,17 +33,15 @@ int	cmd_unset(char **envp, char **argv)
 	i = 0;
 	while (argv[++i])
 		if (check_argv(argv[i]) == 0 && ft_strchr(argv[i], '=') == NULL)	//오류있는 인자는 넘어가기
-			envp = unset_del(envp, argv[i]);
+			unset_del(&(sh->envp), argv[i]);
 
-
-/*
 	i = 0;
-	while (envp[i])
+	while (sh->envp[i])
 	{
-		printf("unset %d	:	%s\n", i, envp[i]);
+		printf("unset %d	:	%s\n", i, sh->envp[i]);
 		i++;
 	}
-*/
+
 	return (0);
 }
 
